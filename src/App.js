0e98c9a34a4e0e6daf07635f113b3fdd42606a67
Ryan Tunis <rtunis@niagaracollege.ca>
2017-01-26 19:58:41 -0500

    Finished tutorial.

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import './style/style.css';
+
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+import API from './api.json';
+
+const API_KEY = API.API_KEY;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('guitar');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      // if key is same as variable name this assignment works
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    // can only call this function every 300 ms
+    const search = _.debounce((term) => { this.videoSearch(term), 2000 });
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={search} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { searchTerm: '' };
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <input
+                    value={this.state.searchTerm}
+                    onChange={(e) => this.onInputChange(e.target.value)} />
+            </div>
+        );
+    }
+
+    onInputChange(searchTerm) {
+        this.setState({ searchTerm });
+        this.props.onSearchTermChange(searchTerm);
+    }
+}
+
+export default SearchBar;
